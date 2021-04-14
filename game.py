962d9f3312f8e8ec1a78f8ae5db133b32d8e3740
Kthrnzh <76144205+Kthrnzh@users.noreply.github.com>
2021-04-14 19:56:10 +0700

    Add files via upload

--- game.py
+++ game.py
@@ -3,6 +3,19 @@ import time
 import random
 
 pygame.init()
+
+black     = (  0,   0,   0)   # черный      
+blue      = (  0,   0, 255)   # синий       
+white     = (255, 255, 255)   # белый       
+yellow    = (255, 255,   0)   # желтый   
+red       = (255,   0,   0)   # красный    
+
+
+
+done=False
+clock=pygame.time.Clock()
+
+
 win = pygame.display.set_mode((320, 500))
 
 pygame.display.set_caption(" CAR VS BLOCKS")
@@ -14,7 +27,7 @@ height = 60
 speed = 5
 
 cactus_width = 70
-cactus_height = 20  
+cactus_height = 20
 
 rand = random.uniform(1,3)
 
@@ -27,14 +40,15 @@ else:
 
 cactus_y = 10
 
-
+shot = 0
 
 def draw_cactus():
-	global cactus_x, cactus_y, cactus_width, cactus_height
+	global cactus_x, cactus_y, cactus_width, cactus_height, shot
 	if cactus_y <= 500:
 		win.blit(block,(cactus_x,cactus_y))
-		cactus_y += 4
+		cactus_y += 8
 	else:
+		shot += 1
 		cactus_y = 10
 		rand = random.uniform(1,3)
 
@@ -55,11 +69,13 @@ fon1 = pygame.image.load('fon1.jpg')
 car = pygame.image.load('car.png')
 block = pygame.image.load('block.png')
 
+
 win.blit(fon,(0,0))
 win.blit(car,(x2,353))
 run = True
 while run:
 	pygame.time.delay(90)
+
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -93,7 +109,15 @@ while run:
 	if x2 == cactus_x:
 		if cactus_y >= 325:
 			win.blit(fon1,(0,0))
+
+			fontObj = pygame.font.SysFont('serif', 20)
+			fullstrin = 'Количество набранных очков: ' + str(shot)
+			text = fontObj.render(fullstrin, True, red, black)
+
+			win.blit(text, (30,250))
+
 			pygame.display.update()
+
 			paused = True
 			while paused:
 				for event in pygame.event.get():
@@ -106,7 +130,18 @@ while run:
 
 			pygame.display.update()
 
+	
+    # Тут можно рисовать
 
-	pygame.display.update()
+	fontObj = pygame.font.SysFont('serif', 20)
+	strin = 'Количество очков: ' + str(shot)
+	text = fontObj.render(strin, True, black, white)
+
+
+	#win.blit(fon,(0,0))
+	win.blit(text, (123,1))
+
+    # Рисунок появится после обновления экрана
+	pygame.display.flip()
 
 pygame.quit()
